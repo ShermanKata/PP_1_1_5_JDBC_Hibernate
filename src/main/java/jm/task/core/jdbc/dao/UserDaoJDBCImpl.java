e766f58e1bd027ab78be99429b981bc7535215f4
Black-cobak <dmitry.sergin@gmail..com>
2022-12-07 17:14:52 +0500

    Commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,20 +1,35 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    private static final Statement statement = Util.getStatement();
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        try {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS Users(id bigint, name varchar(50), lastName varchar(50), age tinyint)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("Не удалось создать таблицу");
+        }
     }
 
     public void dropUsersTable() {
-
+        try {
+            statement.executeUpdate("DROP TABLE IF EXISTS Users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("Не удалось создать таблицу");
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,44 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:mysql://localhost:3306/test_schema";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private static Connection connection;
+    private static Statement statement;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Установлено соединение с БД");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("Не удалось подключиться к БД");
+        }
+    }
+    static {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("Не удалось...");
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public static Statement getStatement() {
+        return statement;
+    }
+
 }
